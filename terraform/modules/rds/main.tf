ddab6a165b3b900742a28bed911951156b4a448d
chandrakatore22@gmail.com <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>
2025-06-13 21:51:29 +0530

    test

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -33,3 +33,26 @@ resource "aws_security_group" "rds_sg" {
   }
 }
 
+resource "aws_db_instance" "main" {
+  identifier              = var.rds_instance_identifier
+  allocated_storage       = var.rds_allocated_storage
+  engine                  = var.rds_engine
+  engine_version          = var.rds_engine_version
+  instance_class          = var.rds_instance_class
+  db_name                 = var.rds_db_name
+  username                = var.rds_username
+  password                = var.rds_password
+  db_subnet_group_name    = aws_db_subnet_group.main.name
+  vpc_security_group_ids  = [aws_security_group.rds_sg.id]
+
+  skip_final_snapshot     = var.rds_skip_final_snapshot
+  deletion_protection     = false
+  publicly_accessible     = false
+  multi_az                = false
+
+  tags = {
+    Name = var.rds_instance_identifier
+  }
+}
+
+
