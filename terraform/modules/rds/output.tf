eb7a2582bd32148eb8f28e04761e1bdc3dbb8447
chandrakatore22@gmail.com <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>
2025-06-14 13:43:24 +0530

    Added db files

--- terraform/modules/rds/output.tf
+++ terraform/modules/rds/output.tf
@@ -8,7 +8,3 @@ output "db_name" {
   description = "Database name"
   value       = aws_db_instance.main.db_name
 }
-output "rds_endpoint" {
-  value = aws_db_instance.this.address
-  description = "RDS Endpoint Address"
-}
