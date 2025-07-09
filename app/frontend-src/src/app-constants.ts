f9004176f812decbee31a0d385eb7b3fa2af456e
chandrakatore22@gmail.com <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>
2025-07-10 00:33:32 +0530

    new AWS account Setup

--- app/frontend-src/src/app-constants.ts
+++ app/frontend-src/src/app-constants.ts
@@ -1,4 +1,4 @@
 export class AppConstants{
 
-    static BACKEND_API_URL= 'http://a2724e73f8f6843edb1d85f638559669-781908185.ap-south-1.elb.amazonaws.com/products';
+    static BACKEND_API_URL= 'http://a6719795cfed94af5824a71d49170628-1266000230.ap-south-1.elb.amazonaws.com/products';
 }
