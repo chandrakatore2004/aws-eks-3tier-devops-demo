58edd9569691ecf69447613a41a819a398c19c57
chandrakatore22@gmail.com <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>
2025-07-09 17:39:33 +0530

    test

--- backend/main.tf
+++ backend/main.tf
@@ -3,7 +3,8 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "chandra-terraform-eks-state-s3-bucket"
+  #bucket = "chandra-terraform-eks-state-s3-bucket"
+  bucket = "ck-terraform-eks-state-s3-bucket"
 
   lifecycle {
     prevent_destroy = false
