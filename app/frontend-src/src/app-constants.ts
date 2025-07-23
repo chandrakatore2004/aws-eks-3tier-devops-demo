ac645153d702887d77a7ad78732fdb6e7b8d09b8
chandrakatore22@gmail.com <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>
2025-07-23 18:34:32 +0530

    Ingress problem fix

--- app/frontend-src/src/app-constants.ts
+++ app/frontend-src/src/app-constants.ts
@@ -1,3 +1,3 @@
 export class AppConstants {
-  static BACKEND_API_URL = 'http://a493fdd4e89354f38a9ab88d67b2c937-22862175.ap-south-1.elb.amazonaws.com/products';
+  static BACKEND_API_URL = 'http://a0f01592bf5054b5d8783eb266b38410-1675433971.ap-south-1.elb.amazonaws.com/products';
 }
