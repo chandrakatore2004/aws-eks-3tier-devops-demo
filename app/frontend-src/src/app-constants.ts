3374b7dcab696c4a86c06965a7d72a286e35367f
chandrakatore22@gmail.com <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>
2025-07-30 16:56:43 +0530

    ArgoCD Project code added

--- app/frontend-src/src/app-constants.ts
+++ app/frontend-src/src/app-constants.ts
@@ -1,3 +1,4 @@
 export class AppConstants {
-  static BACKEND_API_URL = 'http://a0f01592bf5054b5d8783eb266b38410-1675433971.ap-south-1.elb.amazonaws.com/products';
+  static BACKEND_API_URL = '/products';
 }
+
