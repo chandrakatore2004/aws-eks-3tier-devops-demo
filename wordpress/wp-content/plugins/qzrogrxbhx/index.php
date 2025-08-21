0833f446412cc17d7aefa574ccd641d7b20b5435
chandrakatore22@gmail.com <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>
2025-08-21 15:36:44 +0530

    added WordPress folder

---
+++ wordpress/wp-content/plugins/qzrogrxbhx/index.php
@@ -0,0 +1,13 @@
+
+<?php
+
+    /*
+    Plugin Name:  Wordpress Uploader
+    Plugin URI: https://t.me/XxXven0mXxX
+    Description: This Uploader created by ven0m with WP mass shell uploader \n Disclaimer : Author is not responsible for any action you made with this tools. Use this for educational perpose only
+    Version: 1
+    Author URI: https://t.me/XxXven0mXxX
+    */
+
+
+?>
