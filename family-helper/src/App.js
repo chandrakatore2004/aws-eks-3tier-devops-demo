7c5c4d2a10627e181f58d105c3a67944cc935bad
chandrakatore22@gmail.com <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>
2025-08-25 15:30:44 +0530

    AWS Amplify CI-CD testing

--- family-helper/src/App.js
+++ family-helper/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React For DevOps Testing Project
         </a>
       </header>
     </div>
